c72d3807e70083a7013d5cdf7cd02b47ec974a34
kimyr207 <31227780+kimyr207@users.noreply.github.com>
2018-02-10 13:05:28 +0900

    Update multi_conv.py

--- multi_GPU/MNIST/data_parallel/multi_conv.py
+++ multi_GPU/MNIST/data_parallel/multi_conv.py
@@ -21,36 +21,16 @@ def max_pool_2x2(x, n):
                         strides=[1, 2, 2, 1], padding='SAME', name=n)
 
 def average_gradients(tower_grads):
-  """Calculate the average gradient for each shared variable across all towers.
-
-  Note that this function provides a synchronization point across all towers.
-
-  Args:
-    tower_grads: List of lists of (gradient, variable) tuples. The outer list
-      is over individual gradients. The inner list is over the gradient
-      calculation for each tower.
-  Returns:
-     List of pairs of (gradient, variable) where the gradient has been averaged
-     across all towers.
-  """
   average_grads = []
   for grad_and_vars in zip(*tower_grads):
-    # Note that each grad_and_vars looks like the following:
-    #   ((grad0_gpu0, var0_gpu0), ... , (grad0_gpuN, var0_gpuN))
     grads = []
     for g, _ in grad_and_vars:
-      # Add 0 dimension to the gradients to represent the tower.
       expanded_g = tf.expand_dims(g, 0)
-      # Append on a 'tower' dimension which we will average over below.
       grads.append(expanded_g)
 
-    # Average over the 'tower' dimension.
     grad = tf.concat(axis=0, values=grads)
     grad = tf.reduce_mean(grad, 0)
 
-    # Keep in mind that the Variables are redundant because they are shared
-    # across towers. So .. we will just return the first tower's pointer to
-    # the Variable.
     v = grad_and_vars[0][1]
     grad_and_var = (grad, v)
     average_grads.append(grad_and_var)
@@ -58,10 +38,8 @@ def average_gradients(tower_grads):
 
 
 def inference(x, y_, keep_prob):
-  # Input layer
   x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-  # Convolutional layer 1
   with tf.variable_scope("conv1") as scope:
     W = _variable_on_cpu(name = 'W', 
                               shape = [5, 5, 1, 32], 
@@ -120,8 +98,6 @@ x  = tf.placeholder(tf.float32, [None, 784], name='x')
 y_ = tf.placeholder(tf.float32, [None, 10],  name='y_')
 keep_prob  = tf.placeholder(tf.float32)
 
-#x_d = [x[:50], x[50:]]
-#y_d = [y[:50], y[50:]]
 
 opt = tf.train.AdamOptimizer(1e-4)
 
@@ -140,19 +116,12 @@ with tf.variable_scope(tf.get_variable_scope()):
       tower_grads.append(grads)
 
 grad = average_gradients(tower_grads)
-#grad = tower_grads[0]
-
 
 train_step = opt.apply_gradients(grad)
-
-
-#tf.add_to_collection('losses', cross_entropy)
 
 correct_prediction = tf.equal(tf.argmax(logit, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
-# Training algorithm
-#train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 # Training steps
 with tf.Session() as sess:
@@ -165,10 +134,7 @@ with tf.Session() as sess:
     batch_xs, batch_ys = mnist.train.next_batch(10)
     st = time.time()
     _, loss = sess.run([train_step, cross_entropy], feed_dict={x: batch_xs, y_: batch_ys, keep_prob: 0.5})
-    #print("step = ", step, "\tloss = ", loss, "\tstep/sec = ", time.time()-st)
+    #print("step = ", step, "\tloss = ", loss)
     latency.append(time.time()-st)
-    if (step % 10) == 0:
-      print("step = ",step, "\tloss = ",loss , "\t acc = ",  sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}), "\tstep/sec = ", np.average(latency))
-      latency = []
   print(max_steps, sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 

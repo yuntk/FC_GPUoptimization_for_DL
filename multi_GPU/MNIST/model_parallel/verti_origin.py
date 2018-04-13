c42de0cc70df7f36816813f31988c88f8b4d5720
kimyr207 <31227780+kimyr207@users.noreply.github.com>
2018-04-14 00:29:40 +0900

    Add files via upload

---
+++ multi_GPU/MNIST/model_parallel/verti_origin.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+
+
+from tensorflow.examples.tutorials.mnist import input_data
+import time
+import numpy as np
+mnist = input_data.read_data_sets("/home/deepl/FC/multi-GPU/MNIST/data_mnist", one_hot=True)
+
+import tensorflow as tf
+
+x = tf.placeholder(tf.float32, [None, 784])
+y_ = tf.placeholder(tf.float32, [None, 10])
+ 
+#with tf.device('/cpu:0'):
+with tf.variable_scope("layer0"):
+ W = tf.Variable(tf.truncated_normal([784, 8192], stddev=0.1), name="W")
+ b = tf.Variable(tf.truncated_normal([8192], stddev=0.1), name="b")
+
+ local0 = tf.nn.relu(tf.matmul(x, W) + b)
+
+  #local0 = tf.nn.relu(tf.matmul(x, W) + b)
+
+
+with tf.variable_scope("layer1"):
+  W = tf.Variable(tf.truncated_normal([8192, 10], stddev=0.1), name="W")
+  b = tf.Variable(tf.truncated_normal([10], stddev=0.1), name="b")
+
+  local1 = tf.nn.softmax(tf.matmul(local0, W)+b)
+
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(local1), reduction_indices=[1]))
+
+opt = tf.train.AdamOptimizer(1e-4)
+grads = opt.compute_gradients(cross_entropy)
+train_step = opt.apply_gradients(grads)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  
+
+  max_steps = 1000
+
+  for step in range(max_steps):
+    batch_xs, batch_ys = mnist.train.next_batch(10)
+    st = time.time()
+    _, loss = sess.run([train_step, cross_entropy], feed_dict={x: batch_xs, y_: batch_ys})
+
